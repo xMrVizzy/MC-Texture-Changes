0e3086222424805bba8aa0b5676f19eaf8cef69b
Alexey Krainev <xmrvizzy@ya.ru>
2019-09-19 01:12:21 +0500

    Update projectx.py

--- projectx.py
+++ projectx.py
@@ -7,7 +7,7 @@ import shutil
 import errno
 
 VERSIONS_JSON = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
-LATEST_VERSION = 1
+LATEST_VERSION = 0
 
 def fetch_json(url):
 	response = requests.get(url)
